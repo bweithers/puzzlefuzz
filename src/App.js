ac19f6ef3a3c93c8e6fc3a4df9a07b8894dde2ee
BWeithers <36247821+bweithers@users.noreply.github.com>
2024-06-22 13:24:27 -0500

    add test board to appjs

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import './App.css';
+import Board from './components/Board';
 
 function App() {
+  const words = [
+    'Word1', 'Word2', 'Word3', 'Word4', 'Word5',
+    'Word6', 'Word7', 'Word8', 'Word9', 'Word10',
+    'Word11', 'Word12', 'Word13', 'Word14', 'Word15',
+    'Word16', 'Word17', 'Word18', 'Word19', 'Word20',
+    'Word21', 'Word22', 'Word23', 'Word24', 'Word25'
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Codenames Game</h1>
       </header>
+      <Board words={words} />
     </div>
   );
 }
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
 
 export default App;
