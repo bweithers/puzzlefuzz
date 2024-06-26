3557f653853911907f03dc35070ae99d0afcb155
BWeithers <36247821+bweithers@users.noreply.github.com>
2024-06-26 13:04:13 -0500

    add firebasejs with env keys

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// src/firebase.js
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
