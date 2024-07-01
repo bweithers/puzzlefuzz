14d1374e824452b3ff11b02cde0c24c306928e25
BWeithers <36247821+bweithers@users.noreply.github.com>
2024-06-30 22:08:38 -0500

    change wordbox onclick to reveal

--- src/components/WordBox.js
+++ src/components/WordBox.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WordBox.css';
 
-const WordBox = ({ word }) => {
+const WordBox = ({ word, color }) => {
+  const [isRevealed, setIsRevealed] = useState(false);
+
+  const handleClick = () => {
+    setIsRevealed(true);
+  };
+
   return (
-    <div className="word-box">
+    <div 
+      className={`word-box ${isRevealed ? color : ''}`} 
+      onClick={handleClick}
+    >
       {word}
     </div>
   );
