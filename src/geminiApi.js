7420e7313ce44cb4be3f6aa7ec3eca86f87613b4
BWeithers <36247821+bweithers@users.noreply.github.com>
2024-07-22 13:39:01 -0500

    gemini api call

---
+++ src/geminiApi.js
@@ -0,0 +1,19 @@
+// src/geminiApi.js
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"
+    , systemInstruction: "You are a clue giver for a game of word association. You will receive a list of words to use and words to avoid. Give back a one word clue that ties together as many of the first list of words as you can, and give back a number of words that is related to. Explain your reasoning in 20 words or fewer. Here is an example response: {'Apple',2, 'Apples are red and sweet.'}." });
+
+
+
+const Request_Clue = async() =>{ 
+    const prompt = "Words to use: ['Woman', 'Car', 'USA']; Words to avoid: ['Cherry', 'Blueberry', 'Strawberry']";
+
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+
+    return result.response.text();
+}
+
+export default Request_Clue;
