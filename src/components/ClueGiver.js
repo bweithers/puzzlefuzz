3dda3a250dec5a27fef13d6ffc545f0c24a7f7ef
BWeithers <36247821+bweithers@users.noreply.github.com>
2024-07-22 13:42:35 -0500

    make apirequest, update sysinstruct

--- src/components/ClueGiver.js
+++ src/components/ClueGiver.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './ClueGiver.css';
+import Request_Clue from '../geminiApi.js'
 
 const ClueGiver = ({ hint }) => {
+  Request_Clue();
   return (
     <div className="clue-giver">
       <h2>ClueGiver Hint</h2>
--- src/geminiApi.js
+++ src/geminiApi.js
@@ -3,7 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"
-    , systemInstruction: "You are a clue giver for a game of word association. You will receive a list of words to use and words to avoid. Give back a one word clue that ties together as many of the first list of words as you can, and give back a number of words that is related to. Explain your reasoning in 20 words or fewer. Here is an example response: {'Apple',2, 'Apples are red and sweet.'}." });
+    , systemInstruction: "You are a clue giver for a game of word association. You will receive a list of words to use and words to avoid. Give back a one word clue that ties together as many of the first list of words as you can, and give back a number of words that is related to. Explain briefly how each word is related to the clue. Here is an example response: {'Apple',2, 'Apples are red and sweet.'}." });
 
 
 
