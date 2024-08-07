08052182a6fd678d20175e8a4ba9993f12ea0f85
bweithers <bmweithers@gmail.com>
2024-08-07 10:47:16 -0500

    remove createLobby from App

--- src/App.js
+++ src/App.js
@@ -9,26 +9,6 @@ import { collection, doc, setDoc } from 'firebase/firestore';
 import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
 
 
-const createLobby = async () => {
-  // Create a new lobby
-    console.log('Creating lobby...');
-    const lobbyCode = nanoid(6); // Generate a 6-character lobby code
-    const dbRef = collection(firestore, 'game-lobbies');
-    try {
-      const documentRef = doc(dbRef, lobbyCode);
-      await setDoc(documentRef, {
-        LobbyCode: lobbyCode,
-        CreatedAt: new Date(),
-        words: []
-      });
-  
-      console.log('Lobby created:', documentRef);
-      return lobbyCode;
-    } catch (error) {
-      console.error('Error creating lobby:', error);
-      return null;
-    }
-  };
 
   function App() {
     const [lobbyCode, setLobbyCode] = useState(null);
