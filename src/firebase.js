375ea426962e842a0c58b6e556ee0ecad9d3f31e
bweithers <bmweithers@gmail.com>
2026-02-20 16:15:50 -0600

    remove now deleted api key. wtf claude.

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@ import {initializeApp}  from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3DTe_zH3TdDTdHZuXrLzAsjcrodMYSps",
   authDomain: "puzzlefuzz-5ce03.firebaseapp.com",
   projectId: "puzzlefuzz-5ce03",
   appId: "1:1034476288735:web:f99b413a7543117668c76a",
