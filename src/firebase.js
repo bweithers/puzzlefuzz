69357320b3aea688174d392b8c40f4cbf54888a8
Brian <bmweithers@gmail.com>
2026-02-22 14:25:08 -0600

    Add Firebase Public API key to route requests

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
+  apiKey: "AIzaSyB3DTe_zH3TdDTdHZuXrLzAsjcrodMYSps",
   authDomain: "puzzlefuzz-5ce03.firebaseapp.com",
   projectId: "puzzlefuzz-5ce03",
   appId: "1:1034476288735:web:f99b413a7543117668c76a",
